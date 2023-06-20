31d1257574416becfcbf117e75db97b14fbcbd61
LindseyIsaac <isaaclindsey89@gmail.com>
2023-06-19 19:54:34 -0500

    schema

--- controllers/index.js
+++ controllers/index.js
@@ -4,19 +4,7 @@ const homeRoutes = require("./homeroutes.js");
 const dashboardRoutes = require("./dashboard-routes.js");
 
 router.use("/", homeRoutes);
-router.use("/api", apiRoutes);
 router.use("/dashboard", dashboardRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
-
-// const router = require('express').Router();
-
-// const apiRoutes = require('./api');
-// const homeRoutes = require('./home-routes');
-// // const dashRoutes = require('./dashboard-routes');
-
-// router.use('/', homeRoutes);
-// // router.use('/dashboard', dashRoutes);
-// router.use('/api', apiRoutes);
-
-// module.exports = router;
